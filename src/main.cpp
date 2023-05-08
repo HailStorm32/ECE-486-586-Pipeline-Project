8b9e8f370d304e2502c04d3d305a3be3a71128c5
CompileMyLife <ibrah5@pdx.edu>
2023-05-07 18:40:17 -0700

    test out if build system working correctly

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
+#include "../include/sys_core.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    Sys_Core sys_core;
+
+    std::cout << "Hello World!" << std::endl;
 
 	return 0;
 }
