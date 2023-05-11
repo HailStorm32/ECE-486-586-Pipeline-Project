ab3c21c193bad67e7b6b3004a576db18614a9b9f
CompileMyLife <ibrah5@pdx.edu>
2023-05-11 00:15:56 -0700

    print out values of initialized variables for test

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-#include "decoder.h"
-#include "sys_core.h"
+#include "../include/decoder.h"
+#include "../include/sys_core.h"
 
 int main(int argc, char *argv[])
 {
     Sys_Core sys_core;
 
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Sys_Core.PC = " << sys_core.PC << '\n'; 
+    
+    for (int i = 0; i < 32; i++){
+        std::cout << "Sys_Core.reg = " << sys_core.reg[i] << '\n';
+    }
+
+    std::cout << "Sys_Core.clk = " << sys_core.clk << '\n';
 
 	return 0;
 }
