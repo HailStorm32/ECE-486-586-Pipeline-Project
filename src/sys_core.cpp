40e49eea19e1ae79fe1e06a8474e83806ea1388e
CompileMyLife <ibrah5@pdx.edu>
2023-05-11 00:13:53 -0700

    initialized `clk`, `PC`, `reg[]` elements to 0s

--- src/sys_core.cpp
+++ src/sys_core.cpp
@@ -1,3 +1,20 @@
-#include <iostream>
+#include <cstring>
+
 #include "../include/sys_core.h"
 
+// Core Constructor: Initialize variables and arrays to 0s
+Sys_Core::Sys_Core(){
+    PC = 0;
+    memset(reg, 0, sizeof(reg));
+    clk = 0;
+}
+
+// Memory Read method: 
+uint32_t Sys_Core::mem_read(){
+
+}
+
+// Memory Write method:
+uint32_t Sys_Core::mem_write(){
+
+}
