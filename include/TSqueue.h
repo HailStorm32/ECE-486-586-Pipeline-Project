8af2e425648fcd77a5a1e05da1a17412d2cefe0f
Demetri <demetriv.s.7@gmail.com>
2023-05-17 16:41:52 -0700

    Fixed linker error with `TSqueue`

--- include/TSqueue.h
+++ include/TSqueue.h
@@ -1,7 +1,9 @@
 //Adapted from https://www.geeksforgeeks.org/implement-thread-safe-queue-in-c/
 
 #pragma once
+#include <condition_variable>
 #include <queue>
+#include <mutex>
 
 template <typename T>
 class TSQueue {
@@ -22,3 +24,40 @@ public:
     // Pops an element off the queue
     T pop();
 };
+
+template<typename T>
+inline void TSQueue<T>::push(T item)
+{
+    // Acquire lock
+    std::unique_lock<std::mutex> lock(mMutex);
+
+    // Add item
+    mQueue.push(item);
+
+    //// Notify one thread that
+    //// is waiting
+    //mCond.notify_one();
+}
+
+template<typename T>
+inline T TSQueue<T>::pop()
+{
+    // acquire lock
+    std::unique_lock<std::mutex> lock(mMutex);
+
+    //// wait until queue is not empty
+    //mCond.wait(lock,
+    //    [this]() { return !mQueue.empty(); });
+
+    if (mQueue.empty())
+    {
+        return NULL;
+    }
+
+    // retrieve item
+    T item = mQueue.front();
+    mQueue.pop();
+
+    // return item
+    return item;
+}
