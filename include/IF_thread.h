5a6b60326fa9ed014594cb8c8a70d1147b76c2ea
CompileMyLife <ibrah5@pdx.edu>
2023-05-18 00:15:51 -0700

    `IFthread()` function implemented

---
+++ include/IF_thread.h
@@ -0,0 +1,8 @@
+#ifndef __IF_THREAD_H
+#define __IF_THREAD_H
+
+#include "sys_core.h"
+
+void IFthread(SysCore& core);
+
+#endif
---
+++ src/IF_thread.cpp
@@ -0,0 +1,42 @@
+#include <thread>
+#include <chrono>
+#include <cstdint>
+#include <climits>
+#include <iostream>
+#include "IF_thread.h"
+
+#define MIN_SLEEP_TIME      200 // ms
+
+void IFthread(SysCore& sysCore){
+    long long prevClkVal = -1;
+    std::chrono::seconds delay(MIN_SLEEP_TIME);
+    
+    uint32_t instruction;
+
+    while (1){
+        // Continue only if CLK has changed and go ahead from master thread
+        if ((prevClkVal < sysCore.clk) && sysCore.stageInfoIF.okToRun) {
+            // read val at given address (PC val) from memory file
+            instruction = sysCore.memRead(sysCore.PC, true);
+
+            // Check if memRead() returns an error and apply flags
+            if (instruction == UINT_MAX){
+                std::cout << "ERROR: [IFthread] SysCore::memRead() return on error" << std::endl;
+                sysCore.stageInfoIF.okToRun = false;
+                continue;
+            }
+
+            // Increment PC by 4 (bytes) aka 32 bits
+            sysCore.PC = sysCore.PC + 4;
+
+            // Record new CLK val 
+            prevClkVal = sysCore.clk;
+
+            // Push the returned instruction to buffer
+            sysCore.IFtoID.push(instruction);
+        }
+
+        // Apply delay of 200 ms
+        std::this_thread::sleep_for(delay);
+    }
+}
