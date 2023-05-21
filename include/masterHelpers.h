b4fec7051985a1027bba268f85cef5c13279a57e
Demetri <demetriv.s.7@gmail.com>
2023-05-21 14:27:31 -0700

    Addded file for helper functions for master

---
+++ include/masterHelpers.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "sys_core.h"
+
+/*
+* Description:
+*	Will check all the stage struct for any reported errors, and return 
+*	where the errors occured in a TBD
+*	
+*
+* Arguments:
+*	 TBD will return info through an argument
+*
+* Return:
+*	true -- errors reported
+*	false -- no errors reported
+*/
+bool checkForErrors();
---
+++ src/masterHelpers.cpp
@@ -0,0 +1,7 @@
+#include "masterHelpers.h"
+
+bool checkForErrors()
+{
+	//Code TBD, will return false for now (no errors reported)
+	return false;
+}
