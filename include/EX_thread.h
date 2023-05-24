7936a957ee4f29428de46a237fade57d921d35d6
victoria-van <72048290+victoria-van@users.noreply.github.com>
2023-05-24 16:15:13 -0700

    execution stage info struct

--- include/EX_thread.h
+++ include/EX_thread.h
@@ -3,6 +3,17 @@
 
 #include "sys_core.h"
 
+typedef struct exInfo
+{
+	uint32_t PC;
+	uint32_t immediate;
+	opcodes opcode;
+	uint32_t Rs;
+	uint32_t Rt;
+	uint32_t updatedPcVal;
+	bool updatePC;
+}exInfo_t, *exInfoPtr_t;
+
 void EXthread(SysCore& core);
 
 #endif
