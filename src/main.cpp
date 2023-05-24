78b246f4b693834b85ab5bc28fed5f9289488a0b
Demetri <demetriv.s.7@gmail.com>
2023-05-23 23:15:44 -0700

    More finetuning of stage running logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>   // researched that chrono header might be the choice 
-                    // since it is portable to Windows and Linux
+// since it is portable to Windows and Linux
 
 #include "decoder.h"
 #include "IF_thread.h"
@@ -14,142 +14,132 @@
 #define CLOCK_PERIOD    100 //in ms
 #define SETTLE_TIME     1   //in ms
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    // Initialize system core
-    SysCore sysCore("given_sample_memory_image.txt");
+	// Initialize system core
+	SysCore sysCore("given_sample_memory_image.txt");
 
-    // Start threads (passing each thread a ref to the system core)
-    std::thread ifThread(IFthread, std::ref(sysCore));
-    std::thread idThread(IDthread, std::ref(sysCore));
-    std::thread exThread(EXthread, std::ref(sysCore));
-    std::thread memThread(MEMthread, std::ref(sysCore));
+	// Start threads (passing each thread a ref to the system core)
+	std::thread ifThread(IFthread, std::ref(sysCore));
+	std::thread idThread(IDthread, std::ref(sysCore));
+	std::thread exThread(EXthread, std::ref(sysCore));
+	std::thread memThread(MEMthread, std::ref(sysCore));
 
-    std::chrono::milliseconds delay(CLOCK_PERIOD);
+	std::chrono::milliseconds delay(CLOCK_PERIOD);
 
-    std::list<stageThreadPtr_t>* errorsList = NULL;
+	std::list<stageThreadPtr_t>* errorsList = NULL;
 
-    //Start master loop
-    while (true)
-    {
-        std::cout << "DEBUG: [Masterthread] Clock: " << sysCore.clk << std::endl;
+	//Start master loop
+	while (true)
+	{
+		std::cout << "DEBUG: [Masterthread] Clock: " << sysCore.clk << std::endl;
 
-        errorsList = checkForErrors(sysCore);
+		errorsList = checkForErrors(sysCore);
 
-        if (errorsList == NULL)
-        {
-            std::cerr << "\n\nERROR: [Masterthread] errorsList is NULL\n\n" << std::endl;
-            continue;
-        }
-        
-        //Check of reported errors
-        if (!errorsList->empty())
-        {
-            //Process the errors
-            int ret = processError(sysCore, errorsList);
+		if (errorsList == NULL)
+		{
+			std::cerr << "\n\nERROR: [Masterthread] errorsList is NULL\n\n" << std::endl;
+			continue;
+		}
 
-            //TODO: add code for processing errors
-            
-            //We had a HALT, so exit
-            if (ret == 1)
-            {
-                exit(0);
-            }
-        }
-        else if (sysCore.stallsRemaining.isInStall) 
-        {
-            //Only start stages that are not in a stall
+		//Check reported errors
+		if (!errorsList->empty())
+		{
+			//Process the errors
+			int ret = processError(sysCore, errorsList);
 
-            //IF stage
-            if (sysCore.stallsRemaining.stallsRemainIF == 0)
-            {
-                sysCore.stageInfoIF.okToRun = true;
-            }
-            else
-            {
-                //Decrement the stall count
-                sysCore.stallsRemaining.stallsRemainIF--;
-            }
-            
-            //ID stage
-            if (sysCore.stallsRemaining.stallsRemainID == 0)
-            {
-                sysCore.stageInfoID.okToRun = true;
-            }
-            else
-            {
-                //Decrement the stall count
-                sysCore.stallsRemaining.stallsRemainID--;
-            }
-            
-            //EX stage
-            if (sysCore.stallsRemaining.stallsRemainEX == 0)
-            {
-                sysCore.stageInfoEX.okToRun = true;
-            }
-            else
-            {
-                //Decrement the stall count
-                sysCore.stallsRemaining.stallsRemainEX--;
-            }
+			//TODO: add code for processing errors
 
-            //MEM stage
-            if (sysCore.stallsRemaining.stallsRemainMEM == 0)
-            {
-                sysCore.stageInfoMEM.okToRun = true;
-            }
-            else
-            {
-                //Decrement the stall count
-                sysCore.stallsRemaining.stallsRemainMEM--;
-            }
+			//We had a HALT, so exit
+			if (ret == 1)
+			{
+				exit(0);
+			}
+		}
 
-            //WB stage
-            if (sysCore.stallsRemaining.stallsRemainWB == 0)
-            {
-                sysCore.stageInfoWB.okToRun = true;
-            }
-            else
-            {
-                //Decrement the stall count
-                sysCore.stallsRemaining.stallsRemainWB--;
-            }
-            
-        }
-        else
-        {
-            //No errors reported, continue as normal
+		/*//Give the go ahead to all stages
+		sysCore.stageInfoIF.okToRun = true;
+		sysCore.stageInfoID.okToRun = true;
+		sysCore.stageInfoEX.okToRun = true;
+		sysCore.stageInfoMEM.okToRun = true;
+		sysCore.stageInfoWB.okToRun = true;*/
 
-            //Give the go ahead to all stages
-            sysCore.stageInfoIF.okToRun = true;
-            sysCore.stageInfoID.okToRun = true;
-            sysCore.stageInfoEX.okToRun = true;
-            sysCore.stageInfoMEM.okToRun = true;
-            sysCore.stageInfoWB.okToRun = true;
+		//Give the go ahead only to stages that are not stalled
 
+		//IF stage
+		if (sysCore.stallsRemaining.stallsRemainIF == 0)
+		{
+			sysCore.stageInfoIF.okToRun = true;
+		}
+		else
+		{
+			//Decrement the stall count
+			sysCore.stallsRemaining.stallsRemainIF--;
+		}
 
-            //Increment the clock
-            sysCore.clk++;
+		//ID stage
+		if (sysCore.stallsRemaining.stallsRemainID == 0)
+		{
+			sysCore.stageInfoID.okToRun = true;
+		}
+		else
+		{
+			//Decrement the stall count
+			sysCore.stallsRemaining.stallsRemainID--;
+		}
 
-           
-        }
+		//EX stage
+		if (sysCore.stallsRemaining.stallsRemainEX == 0)
+		{
+			sysCore.stageInfoEX.okToRun = true;
+		}
+		else
+		{
+			//Decrement the stall count
+			sysCore.stallsRemaining.stallsRemainEX--;
+		}
 
-        delete errorsList;
-        std::this_thread::sleep_for(delay);
-    }
+		//MEM stage
+		if (sysCore.stallsRemaining.stallsRemainMEM == 0)
+		{
+			sysCore.stageInfoMEM.okToRun = true;
+		}
+		else
+		{
+			//Decrement the stall count
+			sysCore.stallsRemaining.stallsRemainMEM--;
+		}
 
-    //while (1){
-        // if !stall:
-        //  let all threads run
-        //
-        // if stall:
-        //  determine which threads should run
-        //  notify the threads can run and those that can't
-        //  
-        // sysCore.clk++;
-        //
-        // sleep for x time
-    //}
+		//WB stage
+		if (sysCore.stallsRemaining.stallsRemainWB == 0)
+		{
+			sysCore.stageInfoWB.okToRun = true;
+		}
+		else
+		{
+			//Decrement the stall count
+			sysCore.stallsRemaining.stallsRemainWB--;
+		}
+
+		//Increment the clock
+		sysCore.clk++;
+
+		delete errorsList;
+		std::this_thread::sleep_for(delay);
+	}
+
+	//while (1){
+		// if !stall:
+		//  let all threads run
+		//
+		// if stall:
+		//  determine which threads should run
+		//  notify the threads can run and those that can't
+		//  
+		// sysCore.clk++;
+		//
+		// sleep for x time
+	//}
 
 	return 0;
 }
