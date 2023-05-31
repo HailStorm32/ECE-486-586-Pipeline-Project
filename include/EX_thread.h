44113eb7e9198a175cebbe753123980bd2aab743
victoria-van <72048290+victoria-van@users.noreply.github.com>
2023-05-31 15:32:42 -0700

    removed unsused code

--- include/EX_thread.h
+++ include/EX_thread.h
@@ -6,17 +6,6 @@
 #define BITMASK 0x8000
 #define SIGNEXTEND 0xFFFF0000
 
-typedef struct exInfo
-{
-	uint32_t PC;
-	uint32_t immediate;
-	opcodes opcode;
-	uint32_t Rs;
-	uint32_t Rt;
-	uint32_t updatedPcVal;
-	bool updatePC;
-}exInfo_t, *exInfoPtr_t;
-
 void EXthread(SysCore& core);
 
 #endif
